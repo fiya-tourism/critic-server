4a4b272196e992911ea521b11f71488cd1e1186a
e <17615201607@163.com>
2019-07-17 15:38:40 +0800

    s

---
+++ src/main/java/com/fy/travelscritic/TravelsCriticServerApplication.java
@@ -0,0 +1,13 @@
+package com.fy.travelscritic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsCriticServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelsCriticServerApplication.class, args);
+    }
+
+}
